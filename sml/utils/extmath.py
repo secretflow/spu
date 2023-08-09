dec3baf4a97c8da7545e870fe399c4925e18d160
tarantula-leo <54618933+tarantula-leo@users.noreply.github.com>
2023-08-09 20:27:41 +0800

    Create extmath.py

---
+++ sml/utils/extmath.py
@@ -0,0 +1,81 @@
+import jax.numpy as jnp
+import numpy as np
+from jax import random
+
+def Qr_Householder(A):
+    return jnp.linalg.qr(A)
+
+def Qr_Gram_schmidt(A):
+    Q=jnp.zeros_like(A)
+    cnt = 0
+    for a in A.T:
+        u = jnp.copy(a)
+        for i in range(0, cnt):
+            u -= jnp.dot(jnp.dot(Q[:, i].T, a), Q[:, i])
+        e = u / jnp.linalg.norm(u)
+        Q = Q.at[:,cnt].set(e)
+        cnt += 1
+    return Q
+
+def eigh_power(A, max_iter):
+    n = A.shape[0]
+    eig_val = []
+    eig_vec = []
+    for _ in range(n):
+        vec = jnp.ones((n,))
+        for _ in range(max_iter):
+            vec = jnp.dot(A, vec)
+            vec /= jnp.linalg.norm(vec)
+        eigval = jnp.dot(vec.T, jnp.dot(A, vec))
+        eig_vec.append(vec)
+        eig_val.append(eigval)
+        A -= eigval * jnp.outer(vec, vec)
+    eig_vecs = jnp.column_stack(eig_vec)
+    eig_vals = jnp.array(eig_val)
+    return eig_vals, eig_vecs
+
+def eigh_qr(A, max_iter):
+    qr = []
+    n = A.shape[0]
+    Q = jnp.eye(n)
+    for _ in range(max_iter):
+        qr = jnp.linalg.qr(A)
+        Q = jnp.dot(Q,qr[0])
+        A = jnp.dot(qr[1], qr[0])
+    AK = jnp.dot(qr[0], qr[1])
+    eig_val = jnp.diag(AK)
+    eig_vec = Q
+    return eig_val, eig_vec
+
+def rsvd_iteration(A, Omega, scale, power_iter):
+    # iter = 7 if rank < 0.1 * min(A.shape) else 4
+    Y = jnp.dot(A, Omega)
+
+    for _ in range(power_iter):
+        Y = jnp.dot(A, jnp.dot(A.T, Y))
+    Q = Qr_Gram_schmidt(Y / scale)
+    return Q
+
+def svd(A, eigh_iter):
+    m, n = A.shape
+    sigma, U = eigh_power(jnp.dot(A, A.T), eigh_iter)
+    sigma_sqrt = jnp.sqrt(sigma)
+    sigma_inv = jnp.diag((1/sigma_sqrt))
+    V = jnp.dot(jnp.dot(sigma_inv, U.T), A)
+    return (U, sigma_sqrt, V)
+
+def randomized_svd(
+    A,
+    n_components,
+    n_iter=4,
+    random_state=0,
+    scale = [10000000,10000],
+    eigh_iter = 100,
+):
+    random_state = np.random.RandomState(0)
+    Omega = random_state.normal(size=(A.shape[1], n_components)) / scale[0]
+    Q = rsvd_iteration(A, Omega, scale[1] ,n_iter)
+    B = jnp.dot(Q.T, A)
+    u_tilde, s, v = svd(B, eigh_iter)
+    u = jnp.dot(Q, u_tilde)
+    return u, s, v
