1201a4b99cb8b5deaa1e5f3c60da8354c8a56811
magic-hya <huangya@asiainfo.com>
2023-08-10 19:26:57 +0800

    [linear-ridge-cholesky]使用 SPU 实现线性回归模型
    Signed-off-by: magic-hya <huangya@asiainfo.com>

---
+++ sml/linear_model/ridge.py
@@ -0,0 +1,128 @@
+# Copyright 2023 Ant Group Co., Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from scipy import linalg
+from enum import Enum
+from scipy import linalg, sparse
+import jax.numpy as jnp
+import jax.scipy as jsci
+import numpy as np
+
+
+class Solver(Enum):
+    SVD = 'svd'  # not supported
+    CHOLESKY = 'cholesky'
+
+
+class Ridge:
+    """Linear least squares with l2 regularization.
+
+    Minimizes the objective function::
+
+    ||y - Xw||^2_2 + alpha * ||w||^2_2
+
+    This model solves a regression model where the loss function is
+    the linear least squares function and regularization is given by
+    the l2-norm. Also known as Ridge Regression or Tikhonov regularization.
+    This estimator has built-in support for multi-variate regression
+    (i.e., when y is a 2d-array of shape (n_samples, n_targets)).
+
+    Parameters
+    ----------
+    alpha : {float}, default=1.0
+        Constant that multiplies the L2 term, controlling regularization
+        strength. `alpha` must be a non-negative float i.e. in `[0, inf)`.
+
+    solver : {'svd', 'cholesky'}, default='cholesky'
+        Solver to use in the computational routines:
+
+        - 'svd' uses a Singular Value Decomposition of X to compute the Ridge
+          coefficients.
+
+        - 'cholesky' uses the standard jax.scipy.linalg.solve function to
+          obtain a closed-form solution via a Cholesky decomposition of
+          dot(X.T, X)
+    """
+
+    def __init__(self, alpha=1.0, solver="lsqr") -> None:
+        self.alpha = alpha
+        self.solver = solver
+
+    def fit(self, x, y):
+        """Fit Ridge regression model.
+
+        Parameters
+        ----------
+        X : {array-like}, shape (n_samples, n_features)
+            Training data.
+
+        y : ndarray of shape (n_samples,)
+            Target values.
+
+        Returns
+        -------
+        self : object
+            Returns an instance of self.
+        """
+        if y.ndim == 1:
+            y = y.reshape(-1, 1)
+        alpha = jnp.asarray(self.alpha, dtype=x.dtype).ravel()
+        print(f"<<<solver: {self.solver}")
+        if self.solver == Solver.SVD.value:
+            self.coef = _solve_svd(x, y, alpha)
+        if self.solver == Solver.CHOLESKY.value:
+            self.coef = _solve_cholesky(x, y, alpha)
+        return self
+
+    def predict(self, x):
+        """
+        Predict using the linear model.
+
+        Parameters
+        ----------
+        X : {array-like}, shape (n_samples, n_features)
+            Input data for prediction.
+
+        Returns
+        -------
+        C : array, shape (n_samples,)
+            Returns predicted values.
+        """
+        a = x
+        b = self.coef.T
+        ret = jnp.dot(a, b)
+        return ret
+
+
+def _solve_svd(x, y, alpha):
+    U, s, Vt = jsci.linalg.svd(x, full_matrices=False)
+    s_nnz = s[:, jnp.newaxis]
+    UTy = jnp.dot(U.T, y)
+    d = s_nnz / (s_nnz**2 + alpha)
+    d_UT_y = d * UTy
+    return jnp.dot(Vt.T, d_UT_y).T
+
+
+def _solve_cholesky(x, y, alpha):
+    # w = inv(X^t X + alpha*Id) * X.T y
+    n_features = x.shape[1]
+
+    A = jnp.dot(x.T, x)
+    Xy = jnp.dot(x.T, y)
+
+    for i in range(n_features):
+        A = A.at[i, i].set(A[i][i] + alpha[0])
+
+    coefs = jsci.linalg.solve(A, Xy, assume_a="pos", overwrite_a=True).T
+    return coefs
---
+++ sml/linear_model/ridge_emul.py
@@ -0,0 +1,71 @@
+'''
+Author: HuangYuAn
+Date: 2023-08-08 14:52:00
+LastEditors: HuangYuAn
+LastEditTime: 2023-08-08 15:59:03
+FilePath: \spu\sml\linear_model\ridge_emul.py
+Description: 
+Copyright (c) 2023 by HuangYuAn, All Rights Reserved. 
+'''
+# Copyright 2023 Ant Group Co., Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import json
+import jax.numpy as jnp
+import examples.python.utils.dataset_utils as dsutil
+import sml.utils.emulation as emulation
+from sml.linear_model.ridge import Ridge
+
+
+# TODO: design the enumation framework, just like py.unittest
+# all emulation action should begin with `emul_` (for reflection)
+def emul_Ridge(mode: emulation.Mode.MULTIPROCESS):
+    def proc(x1, x2, y):
+        model = Ridge(alpha=1.0, solver="cholesky")
+
+        x = jnp.concatenate((x1, x2), axis=1)
+        y = y.reshape((y.shape[0], 1))
+
+        return model.fit(x, y).predict(x)
+
+    def load_data():
+        with open("examples/python/conf/ds_diabetes_basic.json", "r") as f:
+            dataset_config = json.load(f)
+
+        x1, x2, y = dsutil.load_dataset_by_config(dataset_config)
+
+        return x1, x2, y
+
+    try:
+        # bandwidth and latency only work for docker mode
+        emulator = emulation.Emulator(
+            emulation.CLUSTER_ABY3_3PC, mode, bandwidth=300, latency=20
+        )
+        emulator.up()
+
+        # load mock data
+        x1, x2, y = load_data()
+
+        # mark these data to be protected in SPU
+        x1, x2, y = emulator.seal(x1, x2, y)
+
+        # run
+        result = emulator.run(proc)(x1, x2, y)
+        print(result[:10])
+    finally:
+        emulator.down()
+
+
+if __name__ == "__main__":
+    emul_Ridge(emulation.Mode.MULTIPROCESS)
