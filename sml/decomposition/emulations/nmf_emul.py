1e5ccb6879785989db541c7be071d5968a37676a
tarantula-leo <54618933+tarantula-leo@users.noreply.github.com>
2023-08-23 18:19:49 +0800

    Update nmf_emul.py

--- sml/decomposition/emulations/nmf_emul.py
+++ sml/decomposition/emulations/nmf_emul.py
@@ -38,8 +38,74 @@ def emul_nmf(mode: emulation.Mode.MULTIPROCESS):
         W = model.fit_transform(X)
         H = model._components
         X_reconstructed = model.inverse_transform(W)
-        return W, H, X_reconstructed
+        err = model.reconstruction_err_
+        return W, H, X_reconstructed, err
 
+    try:
+        # bandwidth and latency only work for docker mode
+        conf_path = "sml/decomposition/emulations/3pc.json"
+        emulator = emulation.Emulator(conf_path, mode, bandwidth=300, latency=20)
+        emulator.up()
+        # Create a simple dataset and random_matrix
+        X = np.random.randint(1, 100, (1000, 10))
+        X = np.array(X, dtype=float)
+        n_samples, n_features = X.shape
+        n_components = 5
+        random_seed = 0
+        random_state = np.random.RandomState(random_seed)
+        A = random_state.standard_normal(size=(n_components, n_features))
+        B = random_state.standard_normal(size=(n_samples, n_components))
+        l1_ratio = 0.1
+        alpha_W = 0.01
+
+        X_spu = emulator.seal(X)
+        A_spu = emulator.seal(A)
+        B_spu = emulator.seal(B)
+
+        # Run the emulation
+        W, H, X_reconstructed, err = emulator.run(proc1)(X_spu, A_spu, B_spu)
+        print("reconstruction_error: ", err)
+
+        # sklearn
+        model = SklearnNMF(
+            n_components=n_components,
+            init='random',
+            random_state=random_seed,
+            l1_ratio=l1_ratio,
+            solver="mu",
+            alpha_W=alpha_W,
+        )
+        W_Sklearn = model.fit_transform(X)
+        H_Sklearn = model.components_
+        X_reconstructed_Sklearn = model.inverse_transform(W_Sklearn)
+        err = model.reconstruction_err_
+        print("reconstruction_error_sklearn: ", err)
+        assert np.allclose(W_Sklearn, W, atol=5e-1)
+        assert np.allclose(H_Sklearn, H, atol=5e-1)
+        assert np.allclose(X_reconstructed_Sklearn, X_reconstructed, atol=5e-1)
+
+        # sklearn_seperate
+        model = SklearnNMF(
+            n_components=n_components,
+            init='random',
+            random_state=random_seed,
+            l1_ratio=l1_ratio,
+            solver="mu",
+            alpha_W=alpha_W,
+        )
+        model.fit(X)
+        W_Sklearn_seperate = model.transform(X)
+        H_Sklearn_seperate = model.components_
+        X_reconstructed_Sklearn_seperate = model.inverse_transform(W_Sklearn_seperate)
+        print("W_matrix_sklearn_seperate: ", W_Sklearn_seperate)
+        print("H_matrix_sklearn_seperate: ", H_Sklearn_seperate)
+        print("X_reconstructed_sklearn_seperate: ", X_reconstructed_Sklearn_seperate)
+
+    finally:
+        emulator.down()
+
+
+def emul_nmf_seperate(mode: emulation.Mode.MULTIPROCESS):
     def proc2(X, random_matrixA, random_matrixB):
         model = NMF(
             n_components=n_components,
@@ -69,40 +135,18 @@ def emul_nmf(mode: emulation.Mode.MULTIPROCESS):
         random_state = np.random.RandomState(random_seed)
         A = random_state.standard_normal(size=(n_components, n_features))
         B = random_state.standard_normal(size=(n_samples, n_components))
-        l1_ratio = 0
+        l1_ratio = 0.1
         alpha_W = 0.01
 
         X_spu = emulator.seal(X)
         A_spu = emulator.seal(A)
         B_spu = emulator.seal(B)
 
-        # Run the emulation
-        W, H, X_reconstructed = emulator.run(proc1)(X, A, B)
-        print("W_matrix_spu: ", W)
-        print("H_matrix_spu: ", H)
-        print("X_reconstructed_spu: ", X_reconstructed)
-
         # Run the emulation_seperate
-        W_seperate, H_seperate, X_reconstructed_seperate = emulator.run(proc2)(X, A, B)
-        print("W_matrix_spu_seperate: ", W_seperate)
-        print("H_matrix_spu_seperate: ", H_seperate)
-        print("X_reconstructed_spu_seperate: ", X_reconstructed_seperate)
-
-        # sklearn
-        model = SklearnNMF(
-            n_components=n_components,
-            init='random',
-            random_state=random_seed,
-            l1_ratio=l1_ratio,
-            solver="mu",
-            alpha_W=alpha_W,
-        )
-        W_Sklearn = model.fit_transform(X)
-        H_Sklearn = model.components_
-        X_reconstructed_Sklearn = model.inverse_transform(W_Sklearn)
-        print("W_matrix_sklearn: ", W_Sklearn)
-        print("H_matrix_sklearn: ", H_Sklearn)
-        print("X_reconstructed_sklearn: ", X_reconstructed_Sklearn)
+        W_seperate, H_seperate, X_reconstructed_seperate = emulator.run(proc2)(X_spu, A_spu, B_spu)
+        print("W_matrix_spu_seperate: ", W_seperate[:5,:5])
+        print("H_matrix_spu_seperate: ", H_seperate[:5,:5])
+        print("X_reconstructed_spu_seperate: ", X_reconstructed_seperate[:5,:5])
 
         # sklearn_seperate
         model = SklearnNMF(
@@ -117,9 +161,9 @@ def emul_nmf(mode: emulation.Mode.MULTIPROCESS):
         W_Sklearn_seperate = model.transform(X)
         H_Sklearn_seperate = model.components_
         X_reconstructed_Sklearn_seperate = model.inverse_transform(W_Sklearn_seperate)
-        print("W_matrix_sklearn_seperate: ", W_Sklearn_seperate)
-        print("H_matrix_sklearn_seperate: ", H_Sklearn_seperate)
-        print("X_reconstructed_sklearn_seperate: ", X_reconstructed_Sklearn_seperate)
+        print("W_matrix_sklearn_seperate: ", W_Sklearn_seperate[:5,:5])
+        print("H_matrix_sklearn_seperate: ", H_Sklearn_seperate[:5,:5])
+        print("X_reconstructed_sklearn_seperate: ", X_reconstructed_Sklearn_seperate[:5,:5])
 
     finally:
         emulator.down()
@@ -127,3 +171,4 @@ def emul_nmf(mode: emulation.Mode.MULTIPROCESS):
 
 if __name__ == "__main__":
     emul_nmf(emulation.Mode.MULTIPROCESS)
+    emul_nmf_seperate(emulation.Mode.MULTIPROCESS)
