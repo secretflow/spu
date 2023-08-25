c6a76fffac5a4fbedd887f0039186e662157bebe
salazar1117 <70342834+salazar1117@users.noreply.github.com>
2023-08-25 17:21:58 +0800

    Update kernels.py

--- sml/gaussian_process/kernels.py
+++ sml/gaussian_process/kernels.py
@@ -1,22 +1,15 @@
 import jax.numpy as jnp
 
-class RBF():
+
+class RBF:
     def __init__(self, length_scale=1.0):
         self.length_scale = length_scale
 
     def __call__(self, X, Y=None):
         if Y == None:
             Y = X
-        K = jnp.zeros((X.shape[0], Y.shape[0]), dtype = jnp.float32)
+        K = jnp.zeros((X.shape[0], Y.shape[0]), dtype=jnp.float32)
         for i in range(X.shape[0]):
             for j in range(Y.shape[0]):
-                K = K.at[i, j].set((jnp.sum((X[i] - Y[j])**2)))
-        return jnp.exp(- K / (2 * self.length_scale))
-
-# X = ([[1,1,1],[2,7,5],[3,2,3],[4,0,1]])
-# X1 = ([[1,1,1],[2,1,5],[3,2,3]])
-# X = jnp.array(X)
-# X1 = jnp.array(X1)
-# a = RBF()
-# print(a(X))
-# print(a(X, X1))
+                K = K.at[i, j].set((jnp.sum((X[i] - Y[j]) ** 2)))
+        return jnp.exp(-K / (2 * self.length_scale))
