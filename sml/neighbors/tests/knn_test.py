f6317123a09598689fb181868139a305996b6a27
hacker-jerry <57279550+hacker-jerry@users.noreply.github.com>
2023-08-28 21:42:47 +0800

    [fix] knn bazel format issue

--- sml/neighbors/tests/knn_test.py
+++ sml/neighbors/tests/knn_test.py
@@ -35,6 +35,7 @@ class UnitTests(unittest.TestCase):
             3, spu_pb2.ProtocolKind.ABY3, spu_pb2.FieldType.FM64
         )
         
+        
         # Test fit_predict
         def proc_predict(
             X_train, y_train, X_test, n_classes, n_neighbors=5, weights='uniform'
