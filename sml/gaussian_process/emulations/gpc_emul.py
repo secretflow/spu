b99f37942f21ccdee6dd206080d482584caf54e2
lianhua <zhoujinjin.zjj@antgroup.com>
2023-10-25 16:36:03 +0800

    gpc-fix

--- sml/gaussian_process/emulations/gpc_emul.py
+++ sml/gaussian_process/emulations/gpc_emul.py
@@ -1,3 +1,17 @@
+# Copyright 2023 Ant Group Co., Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import os
 import sys
 
@@ -11,39 +25,41 @@ from sml.gaussian_process._gpc import GaussianProcessClassifier
 
 
 def emul_gpc(mode: emulation.Mode.MULTIPROCESS):
-    def proc(x, y):
-        model = GaussianProcessClassifier(max_iter_predict=10)
+    def proc(x, y, x_pred):
+        model = GaussianProcessClassifier(max_iter_predict=10, n_classes_=3)
         model.fit(x, y)
 
-        pred = model.predict(x)
+        pred = model.predict(x_pred)
         return pred
 
     try:
         # bandwidth and latency only work for docker mode
         emulator = emulation.Emulator(
-            emulation.CLUSTER_ABY3_3PC, mode, bandwidth=300, latency=20
+            "sml/gaussian_process/emulations/3pc.json", mode, bandwidth=300, latency=20
         )
         emulator.up()
 
-        # load data
+        # Create dataset
         x, y = load_iris(return_X_y=True)
-        x = x[45:55, :]
-        y = y[45:55]
+
+        idx = list(range(45, 55)) + list(range(100, 105))
+        prd_idx = list(range(0, 5)) + list(range(55, 60)) + list(range(110, 115))
+        x_pred = x[prd_idx, :]
+        y_pred = y[prd_idx]
+        x = x[idx, :]
+        y = y[idx]
 
         # mark these data to be protected in SPU
-        x, y = emulator.seal(x, y)
-        result = emulator.run(proc)(x, y)
-        print("Accuracy: ", jnp.sum(result == y) / len(y))
+        x, y, x_pred = emulator.seal(x, y, x_pred)
+        result = emulator.run(proc)(x, y, x_pred)
+
+        print(result)
+        print(y_pred)
+        print("Accuracy: ", jnp.sum(result == y_pred) / len(y_pred))
 
     finally:
         emulator.down()
 
-
-if __name__ == "__main__":
-    emul_gpc(emulation.Mode.MULTIPROCESS)
-
-    finally:
-        emulator.down()
 
 if __name__ == "__main__":
     emul_gpc(emulation.Mode.MULTIPROCESS)
