04328c0b665d902f10bce5e2e76005cfdfbc1849
周金金 <zhoujinjin.zjj@antgroup.com>
2023-10-25 22:25:01 +0800

    gpc-fix (#375)
    # Pull Request
    
    ## What problem does this PR solve?
    
    Fix the problem of GPC before.
    
    ## Possible side effects?
    
    - Performance:
    
    - Backward compatibility:

--- sml/gaussian_process/_gpc.py
+++ sml/gaussian_process/_gpc.py
@@ -1,16 +1,25 @@
-import os
-import sys
+# Copyright 2023 Ant Group Co., Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-import jax
 import jax.numpy as jnp
 from jax import grad
 from jax.lax.linalg import cholesky
 from jax.scipy.linalg import cho_solve, solve
 from jax.scipy.special import erf, expit
 
-sys.path.append(os.path.join(os.path.dirname(__file__), "./"))
-from kernels import RBF
-from ovo_ovr import OneVsRestClassifier
+from .kernels import RBF
+from .ovo_ovr import OneVsRestClassifier
 
 LAMBDAS = jnp.array([0.41, 0.4, 0.37, 0.44, 0.39])[:, jnp.newaxis]
 COEFS = jnp.array(
@@ -31,7 +40,7 @@ class _BinaryGaussianProcessClassifierLaplace:
         self.poss = poss
 
     def fit(self, X, y):
-        self._check_kernal()
+        self._check_kernel()
 
         self.X_train_ = jnp.asarray(X)
 
@@ -46,51 +55,35 @@ class _BinaryGaussianProcessClassifierLaplace:
         self.y_train = y
 
         K = self.kernel_(self.X_train_)
-        self.f_ = self._posterior_mode(K)
-        return self.f_
 
-    # def log_and_grad(self, f, y_train):
-    #     _tmp = lambda f, y_train: jnp.sum(self.approx_func(y_train*f))
-    #     return grad(_tmp)(f, y_train)/self.approx_func(y_train*f)
-
-    # def log_and_2grads_and_negtive(self, f, y_train):
-    #     _tmp = lambda f, y: jnp.sum(self.log_and_grad(f, y))
-    #     return -grad(_tmp)(f, y_train)
-
-    # def log_and_3grads(self, f, y_train):
-    #     _tmp = lambda f, y_train: jnp.sum(-self.log_and_2grads_and_negtive(f, y_train))
-    #     return grad(_tmp)(f, y_train)
+        (
+            self.pi_,
+            self.W_sr_,
+            self.L_,
+        ) = self._posterior_mode(K)
+        return self
 
     def predict(self, Xll):
         X = jnp.asarray(Xll)
         K_star = self.kernel_(self.X_train_, X)
         # f_star = K_star.T.dot(self.log_and_grad(self.f_, self.y_train))
-        f_star = K_star.T.dot(self.y_train - self.approx_func(self.f_))
+        f_star = K_star.T.dot(self.y_train - self.pi_)
 
         return jnp.where(f_star > 0, 1, 0)
 
     def predict_proba(self, Xll):
         X = jnp.asarray(Xll)
-        K = self.kernel_(self.X_train_)
-        # Based on Algorithm 3.2 of GPML
-
-        # W = self.log_and_2grads_and_negtive(self.f_, self.y_train)
-        pi = self.approx_func(self.f_)
-        W = pi * (1 - pi)
-
-        W_sqr = jnp.sqrt(W)
-        W_sqr_K = W_sqr[:, jnp.newaxis] * K
-        B = jnp.eye(W.shape[0]) + W_sqr_K * W_sqr
-        L = cholesky(B)
 
         K_star = self.kernel_(self.X_train_, X)
         # f_star = K_star.T.dot(self.log_and_grad(self.f_, self.y_train))
-        f_star = K_star.T.dot(self.y_train - pi)
-        v = solve(L, W_sqr[:, jnp.newaxis] * K_star)
-        var_f_star = jnp.diag(self.kernel_(X)) - jnp.einsum("ij,ij->j", v, v)
+        f_star = K_star.T.dot(self.y_train - self.pi_)
+        v = solve(self.L_, self.W_sr_[:, jnp.newaxis] * K_star)
+        # var_f_star = jnp.diag(self.kernel_(X)) - jnp.einsum("ij,ij->j", v, v)
+        var_f_star = self.kernel_.diag(X) - jnp.einsum("ij,ij->j", v, v)
 
         alpha = 1 / (2 * var_f_star)
         gamma = LAMBDAS * f_star
+
         integrals = (
             jnp.sqrt(jnp.pi / alpha)
             * erf(gamma * jnp.sqrt(alpha / (alpha + LAMBDAS**2)))
@@ -103,8 +96,8 @@ class _BinaryGaussianProcessClassifierLaplace:
     def _posterior_mode(self, K):
         # Based on Algorithm 3.1 of GPML
         f = jnp.zeros_like(
-                self.y_train, dtype=jnp.float32
-            )  # a warning is triggered if float64 is used
+            self.y_train, dtype=jnp.float32
+        )  # a warning is triggered if float64 is used
 
         for _ in range(self.max_iter_predict):
             # W = self.log_and_2grads_and_negtive(f, self.y_train)
@@ -114,7 +107,7 @@ class _BinaryGaussianProcessClassifierLaplace:
             W_sqr_K = W_sqr[:, jnp.newaxis] * K
 
             B = jnp.eye(W.shape[0]) + W_sqr_K * W_sqr
-            L = cholesky(B)
+            L = cholesky(B, symmetrize_input=False)
             # b = W * f + self.log_and_grad(f, self.y_train)
             b = W * f + (self.y_train - pi)
             a = b - jnp.dot(
@@ -123,20 +116,23 @@ class _BinaryGaussianProcessClassifierLaplace:
             )
             f = K.dot(a)
 
-        self.f_cached = f  # for warm-start
-        return f
+            # no early stop here...
 
-    def _check_kernal(self):
+        # for warm-start
+        # self.f_cached = f
+        return pi, W_sqr, L
+
+    def _check_kernel(self):
         if self.kernel is None:  # Use an RBF kernel as default
             self.kernel_ = RBF()
         else:
-            self.kernel_ = self.kernel
+            raise NotImplemented("Only RBF kernel is supported now.")
 
 
 class GaussianProcessClassifier:
     """Gaussian process classification (GPC) based on Laplace approximation.
 
-    The implementation is based on Algorithm 3.1, 3.2, and 5.1 from [RW2006]_.
+    The implementation is based on Algorithm 3.1, 3.2 from [RW2006]_.
 
     Internally, the Laplace approximation is used for approximating the
     non-Gaussian posterior by a Gaussian.
@@ -146,17 +142,20 @@ class GaussianProcessClassifier:
     classifiers are fitted. Note that this class thus does not implement
     a true multi-class Laplace approximation.
 
-    Read more in the :ref:`User Guide <gaussian_process>`.
+    IMPORTANT NOTES:
+      1. Current implementations will not optimize the parameters of kernel during training.
+      2. ONLY RBF kernel is supported now.
+      3. ONLY one_vs_rest mode is supported for multi-class tasks. (You should pre-process your label to 0,1,2... like)
+      4. You MUST specify n_classes_ explicitly, because we can not do data inspections under MPC.
 
     Parameters
     ----------
     kernel : kernel instance, default=None
         The kernel specifying the covariance function of the GP. If None is
-        passed, the kernel "1.0 * RBF(1.0)" is used as default. Note that
-        the kernel's hyperparameters are optimized during fitting. Also kernel
-        cannot be a `CompoundKernel`.
+        passed, the kernel "1.0 * RBF(1.0)" is used as default.
+        Only RBF kernel is supported now.
 
-    max_iter_predict : int, default=100
+    max_iter_predict : int, default=20
         The maximum number of iterations in Newton's method for approximating
         the posterior during predict. Smaller values will reduce computation
         time at the cost of worse results.
@@ -166,44 +165,18 @@ class GaussianProcessClassifier:
         One binary Gaussian process classifier is fitted for each class, which
         is trained to separate this class from the rest.
 
-    poss : "sigmoid", allable or None, default="sigmoid", the predefined 
-        likelihood function which computes the possibility of the predict output 
+    poss : "sigmoid", allable or None, default="sigmoid", the predefined
+        likelihood function which computes the possibility of the predict output
         w.r.t. f value.
 
-    Attributes
-    ----------
-    base_estimator_ : ``Estimator`` instance
-        The estimator instance that defines the likelihood function
-        using the observed data.
-
-    kernel_ : kernel instance
-        The kernel used for prediction. In case of binary classification,
-        the structure of the kernel is the same as the one passed as parameter
-        but with optimized hyperparameters. In case of multi-class
-        classification, a CompoundKernel is returned which consists of the
-        different kernels used in the one-versus-rest classifiers.
-
     n_classes_ : int
-        The number of classes in the training data
+        The number of classes in the training data.
 
     References
     ----------
     .. [RW2006] `Carl E. Rasmussen and Christopher K.I. Williams,
        "Gaussian Processes for Machine Learning",
        MIT Press 2006 <https://www.gaussianprocess.org/gpml/chapters/RW.pdf>`_
-
-    Examples
-    --------
-    >>> from sklearn.datasets import load_iris
-    >>> from sklearn.gaussian_process import GaussianProcessClassifier
-    >>> from sklearn.gaussian_process.kernels import RBF
-    >>> X, y = load_iris(return_X_y=True)
-    >>> kernel = 1.0 * RBF(1.0)
-    >>> gpc = GaussianProcessClassifier(kernel=kernel,
-    ...         random_state=0).fit(X, y, 3)
-    >>> gpc.predict_proba(X[:2,:])
-    array([[0.83548752, 0.03228706, 0.13222543],
-           [0.79064206, 0.06525643, 0.14410151]])
     """
 
     def __init__(
@@ -211,15 +184,23 @@ class GaussianProcessClassifier:
         kernel=None,
         *,
         poss="sigmoid",
-        max_iter_predict=100,
+        max_iter_predict=20,
         multi_class="one_vs_rest",
+        n_classes_=2,
     ):
         self.kernel = kernel
         self.max_iter_predict = max_iter_predict
         self.multi_class = multi_class
         self.poss = poss
 
-    def fit(self, X, y, n_classes_):
+        self.n_classes_ = n_classes_
+        self.base_estimator_ = None
+
+        assert (
+            n_classes_ > 1
+        ), "GaussianProcessClassifier requires 2 or more distinct classes"
+
+    def fit(self, X, y):
         """Fit Gaussian process classification model.
 
         Parameters
@@ -227,25 +208,15 @@ class GaussianProcessClassifier:
         X : jax numpy array (n_samples, n_features) of object
             Feature vectors of training data.
 
-        y : jax numpy array (n_samples,) Target values, 
+        y : jax numpy array (n_samples,) Target values,
         must be preprocessed to 0, 1, 2, ...
-
-        n_classes_ : The number of classes in the training data
 
         Returns
         -------
         self : object
             Returns an instance of self.
         """
-        self.n_classes_ = n_classes_
         self.y_train = jnp.array(y)
-
-        if self.n_classes_ == 1:
-            raise ValueError(
-                "GaussianProcessClassifier requires 2 or more "
-                "distinct classes; got 1 class (only class %s "
-                "is present)" % self.n_classes_[0]
-            )
 
         self.base_estimator_ = _BinaryGaussianProcessClassifierLaplace(
             kernel=self.kernel,
@@ -256,7 +227,11 @@ class GaussianProcessClassifier:
         if self.n_classes_ > 2:
             if self.multi_class == "one_vs_rest":
                 self.base_estimator_ = OneVsRestClassifier(
-                    self.base_estimator_, self.n_classes_
+                    _BinaryGaussianProcessClassifierLaplace,
+                    self.n_classes_,
+                    kernel=self.kernel,
+                    max_iter_predict=self.max_iter_predict,
+                    poss=self.poss,
                 )
             elif self.multi_class == "one_vs_one":
                 raise ValueError("one_vs_one classifier is not supported")
@@ -265,6 +240,8 @@ class GaussianProcessClassifier:
 
         self.X = jnp.array(X)
         self.base_estimator_.fit(self.X, self.y_train)
+
+        return self
 
     def predict(self, X):
         """Perform classification on an array of test vectors X.
@@ -301,18 +278,5 @@ class GaussianProcessClassifier:
         return self.base_estimator_.predict_proba(X)
 
     def check_is_fitted(self):
-        """Perform is_fitted validation for estimator.
-
-        Checks if the estimator is fitted by verifying the presence of
-        fitted attribute self.n_classes_ and otherwise
-        raises a NotFittedError with the given message.
-
-        Raises
-        ------
-        Exception
-            If the attribute is not found.
-        """
-        try: 
-            self.n_classes_
-        except: 
-            raise Exception('Model is not fitted yet')
+        """Perform is_fitted validation for estimator."""
+        assert self.base_estimator_ is not None, "Model should be fitted first."
--- sml/gaussian_process/tests/gpc_test.py
+++ sml/gaussian_process/tests/gpc_test.py
@@ -1,3 +1,17 @@
+# Copyright 2023 Ant Group Co., Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import os
 import sys
 import unittest
@@ -17,27 +31,37 @@ from sml.gaussian_process._gpc import GaussianProcessClassifier
 class UnitTests(unittest.TestCase):
     def test_gpc(self):
         sim = spsim.Simulator.simple(
-            3, spu_pb2.ProtocolKind.ABY3, spu_pb2.FieldType.FM64
+            3, spu_pb2.ProtocolKind.ABY3, spu_pb2.FieldType.FM128
         )
 
         # Test GaussianProcessClassifier
-        def proc(x, y):
-            model = GaussianProcessClassifier(max_iter_predict=10)
-            model.fit(x, y, 3)
+        @jax.jit
+        def proc(x, y, x_pred):
+            model = GaussianProcessClassifier(max_iter_predict=10, n_classes_=3)
+            model = model.fit(x, y)
 
-            pred = model.predict(x)
+            pred = model.predict(x_pred)
             return pred
+            # return model
 
         # Create dataset
         x, y = load_iris(return_X_y=True)
-        # x = x[45:55, :]
-        # y = y[45:55]
+
+        idx = list(range(45, 55)) + list(range(100, 105))
+        prd_idx = list(range(0, 5)) + list(range(55, 60)) + list(range(110, 115))
+        x_pred = x[prd_idx, :]
+        y_pred = y[prd_idx]
+        x = x[idx, :]
+        y = y[idx]
 
         # Run
-        result = spsim.sim_jax(sim, proc)(x, y)
+        result = spsim.sim_jax(sim, proc)(x, y, x_pred)
+        # result = proc(x, y, x_pred)
+
         print(result)
-        print(y)
-        print("Accuracy: ", jnp.sum(result == y) / len(y))
+        print(y_pred)
+        print("Accuracy: ", jnp.sum(result == y_pred) / len(y_pred))
 
 
-unittest.main()
+if __name__ == "__main__":
+    unittest.main()
