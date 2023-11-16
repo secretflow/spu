6df82f35ace47e5dff7ccd6065e2c6e15a6a7868
tarantula-leo <54618933+tarantula-leo@users.noreply.github.com>
2023-11-16 16:52:14 +0800

    Update classification_emul.py

--- sml/metrics/classification/classification_emul.py
+++ sml/metrics/classification/classification_emul.py
@@ -22,7 +22,13 @@ from sklearn import metrics
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../'))
 
 import sml.utils.emulation as emulation
-from sml.metrics.classification.classification import roc_auc_score, f1_score, precision_score, recall_score, accuracy_score
+from sml.metrics.classification.classification import (
+    roc_auc_score,
+    f1_score,
+    precision_score,
+    recall_score,
+    accuracy_score,
+)
 
 
 # TODO: design the enumation framework, just like py.unittest
@@ -50,7 +56,6 @@ def emul_SGDClassifier(mode: emulation.Mode.MULTIPROCESS):
         emulator.down()
 
 
-
 def emul_Classification(mode: emulation.Mode.MULTIPROCESS):
     try:
         # bandwidth and latency only work for docker mode
@@ -59,29 +64,60 @@ def emul_Classification(mode: emulation.Mode.MULTIPROCESS):
         )
         emulator.up()
     
-        def proc(y_true, y_pred, average='binary', labels=None, pos_label=1 ,transform=1):
-            f1 = f1_score(y_true, y_pred, average=average, labels=labels, pos_label=pos_label, transform=transform)
-            precision = precision_score(y_true, y_pred, average=average, labels=labels, pos_label=pos_label, transform=transform)
-            recall = recall_score(y_true, y_pred, average=average, labels=labels, pos_label=pos_label, transform=transform)
+def proc(
+            y_true, y_pred, average='binary', labels=None, pos_label=1, transform=1
+        ):
+            f1 = f1_score(
+                y_true,
+                y_pred,
+                average=average,
+                labels=labels,
+                pos_label=pos_label,
+                transform=transform,
+            )
+            precision = precision_score(
+                y_true,
+                y_pred,
+                average=average,
+                labels=labels,
+                pos_label=pos_label,
+                transform=transform,
+            )
+            recall = recall_score(
+                y_true,
+                y_pred,
+                average=average,
+                labels=labels,
+                pos_label=pos_label,
+                transform=transform,
+            )
             accuracy = accuracy_score(y_true, y_pred)
-            return f1,precision,recall,accuracy
-        
+            return f1, precision, recall, accuracy
+
+
         def sklearn_proc(y_true, y_pred, average='binary', labels=None, pos_label=1):
-            f1 = metrics.f1_score(y_true, y_pred, average=average, labels=labels, pos_label=pos_label)
-            precision = metrics.precision_score(y_true, y_pred, average=average, labels=labels, pos_label=pos_label)
-            recall = metrics.recall_score(y_true, y_pred, average=average, labels=labels, pos_label=pos_label)
+            f1 = metrics.f1_score(
+                y_true, y_pred, average=average, labels=labels, pos_label=pos_label
+            )
+            precision = metrics.precision_score(
+                y_true, y_pred, average=average, labels=labels, pos_label=pos_label
+            )
+            recall = metrics.recall_score(
+                y_true, y_pred, average=average, labels=labels, pos_label=pos_label
+            )
             accuracy = metrics.accuracy_score(y_true, y_pred)
-            return f1,precision,recall,accuracy
-        
+            return f1, precision, recall, accuracy
+
+
         def check(spu_result, sk_result):
-            for  pair in zip(spu_result, sk_result):
+            for pair in zip(spu_result, sk_result):
                 np.testing.assert_allclose(pair[0], pair[1], rtol=1, atol=1e-5)
 
         # Test binary
         y_true = jnp.array([0, 1, 1, 0, 1, 1])
         y_pred = jnp.array([0, 0, 1, 0, 1, 1])
-        spu_result = emulator.run(proc)(y_true, y_pred, pos_label=1, transform=0)
-        sk_result = sklearn_proc(y_true, y_pred)
+        spu_result = emulator.run(proc)(y_true, y_pred, average=None, labels=[0, 1, 2])
+        sk_result = sklearn_proc(y_true, y_pred, average=None, labels=[0, 1, 2])
         check(spu_result, sk_result)
 
         # Test multiclass
