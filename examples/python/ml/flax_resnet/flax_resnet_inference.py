c8d6f5171a2883d504d84e2d9c1afc188cd1f4b7
DuanYuFi <DuanYuFi@foxmail.com>
2023-12-08 06:40:45 +0000

    beautify output from resnet50 inference

--- examples/python/ml/flax_resnet/flax_resnet_inference.py
+++ examples/python/ml/flax_resnet/flax_resnet_inference.py
@@ -31,27 +31,39 @@ model = FlaxResNetForImageClassification.from_pretrained("microsoft/resnet-50")
 
 inputs = processor(image, return_tensors="jax")["pixel_values"]
 
+from time import time
 
-def infer_cipher(inputs, params):
+
+def inference(inputs, params):
     config = AutoConfig.from_pretrained("microsoft/resnet-50")
     model_cipher = FlaxResNetForImageClassification(config=config)
     outputs = model_cipher(pixel_values=inputs, params=params)
     return outputs
 
 
-def main(inputs, model):
+def run_on_spu(inputs, model):
+    start = time()
     inputs = ppd.device("P1")(lambda x: x)(inputs)
     params = ppd.device("P2")(lambda x: x)(model.params)
-    outputs = ppd.device("SPU")(infer_cipher)(inputs, params)
+    outputs = ppd.device("SPU")(inference)(inputs, params)
     outputs = ppd.get(outputs)
     outputs = outputs['logits']
     predicted_class_idx = jax.numpy.argmax(outputs, axis=-1)
+    print(f"Elapsed time:{time() - start}")
+    print("Predicted class:", model.config.id2label[predicted_class_idx.item()])
+
+
+def run_on_cpu(inputs, model):
+    start = time()
+    outputs = inference(inputs, model.params)
+    outputs = outputs['logits']
+    predicted_class_idx = jax.numpy.argmax(outputs, axis=-1)
+    print(f"Elapsed time:{time() - start}")
     print("Predicted class:", model.config.id2label[predicted_class_idx.item()])
 
 
 if __name__ == "__main__":
-    from time import time
-
-    start = time()
-    main(inputs, model)
-    print(f"Elapsed time:{time() - start}")
+    print("Run on CPU\n------\n")
+    run_on_cpu(inputs, model)
+    print("Run on SPU\n------\n")
+    run_on_spu(inputs, model)
