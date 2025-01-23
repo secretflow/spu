d5529289ce8df02827abd52ca77b8e90745233e2
lwxxxxxxx <1433359185@qq.com>
2025-01-23 15:29:59 +0800

    add src/libspu/mpc/swift

--- src/libspu/mpc/factory.cc
+++ src/libspu/mpc/factory.cc
@@ -26,6 +26,8 @@
 #include "libspu/mpc/securenn/protocol.h"
 #include "libspu/mpc/semi2k/io.h"
 #include "libspu/mpc/semi2k/protocol.h"
+#include "libspu/mpc/swift/io.h"
+#include "libspu/mpc/swift/protocol.h"
 
 namespace spu::mpc {
 
@@ -47,6 +49,9 @@ void Factory::RegisterProtocol(
     }
     case ProtocolKind::SECURENN: {
       return regSecurennProtocol(ctx, lctx);
+    }
+    case ProtocolKind::SWIFT: {
+      return regSwiftProtocol(ctx, lctx);
     }
     default: {
       SPU_THROW("Invalid protocol kind {}", ctx->config().protocol());
@@ -71,6 +76,9 @@ std::unique_ptr<IoInterface> Factory::CreateIO(const RuntimeConfig& conf,
     }
     case ProtocolKind::SECURENN: {
       return securenn::makeSecurennIo(conf.field(), npc);
+    }
+    case ProtocolKind::SWIFT: {
+      return swift::makeSwiftIo(conf.field(), npc);
     }
     default: {
       SPU_THROW("Invalid protocol kind {}", conf.protocol());
