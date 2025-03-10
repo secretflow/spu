aed29bde4082d6575ec106d4fb9970daa431089e
HardCodeBaby <hardcode.spu@gmail.com>
2025-03-10 14:09:56 +0800

    repo-sync-2025-03-10T10:33:40+0800 (#1054)

--- src/libspu/version.h
+++ src/libspu/version.h
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#define SPU_VERSION "0.9.4.dev20250227"
+#define SPU_VERSION "0.9.4.dev20250310"
 
 #include <string_view>
 
