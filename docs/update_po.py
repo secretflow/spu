6f4a12618b63e9bfaba306b6faa61a2d4d5b0ffc
w-gc <25614556+w-gc@users.noreply.github.com>
2025-03-13 16:53:54 +0800

    fix

--- docs/update_po.py
+++ docs/update_po.py
@@ -11,13 +11,21 @@ for po_file in PO_FILES:
         dst_ctx = open(f"{ROOT}/{po_file[:-3]}/{sub_po_file}", 'r').read()
 
         for title in re.findall('#: (.*?)\n', dst_ctx):
-            src_start = src_ctx.find('msgstr', src_ctx.find(title))
+            src_title = src_ctx.find(title)
+            if src_title < 0:
+                continue
+
+            dst_title = dst_ctx.find(title)
+            if dst_title < 0:
+                continue
+
+            src_start = src_ctx.find('msgstr', src_title)
             src_end = src_ctx.find('\n', src_start)
 
             if src_end - src_start <= len('msgstr ""'):
                 continue
 
-            dst_start = dst_ctx.find('msgstr', dst_ctx.find(title))
+            dst_start = dst_ctx.find('msgstr', dst_title)
             dst_end = dst_ctx.find('\n', dst_start)
 
             dst_ctx = dst_ctx[:dst_start] + src_ctx[src_start:src_end] + dst_ctx[dst_end:] 
