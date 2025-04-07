bb89a4916d42ad3a2641738026be62842611164f
lwxxxxxxx <1433359185@qq.com>
2025-04-07 11:26:33 +0000

    add mpc/experimental/swift

--- src/libspu/mpc/experimental/swift/protocol_test.cc
+++ src/libspu/mpc/experimental/swift/protocol_test.cc
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "libspu/mpc/swift/protocol.h"
+#include "libspu/mpc/experimental/swift/protocol.h"
 
 #include <mutex>
 
