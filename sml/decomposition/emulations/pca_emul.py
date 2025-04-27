1491924d3d0e329a7cbcc5dea101bf42ed25466e
fdddy <92495150+fdddy@users.noreply.github.com>
2025-04-27 20:35:10 +0800

    Ant project-Jacobi-EVD and Jacobi-PCA (#950)
    # Pull Request
    
    ## What problem does this PR solve?  
    [Feature] Add a new eigendecomposition method called Jacobi and the PCA
    implement based on Jacobi-evd
    
    ---------
    
    Co-authored-by: Candicepan <48274303+Candicepan@users.noreply.github.com>
    Co-authored-by: warriorpaw <zhaoderun.zdr@alibaba-inc.com>

--- sml/decomposition/emulations/pca_emul.py
+++ sml/decomposition/emulations/pca_emul.py
@@ -27,29 +27,23 @@ import sml.utils.emulation as emulation
 from sml.decomposition.pca import PCA
 
 
-def emul_powerPCA(mode: emulation.Mode.MULTIPROCESS):
-    print("start power method emulation.")
+def test_pca(mode: emulation.Mode.MULTIPROCESS):
+    def emul_powerPCA():
+        print("start power method emulation.")
 
-    def proc_transform(X):
-        model = PCA(
-            method='power_iteration',
-            n_components=2,
-            max_power_iter=200,
-        )
+        def proc_transform(X):
+            model = PCA(
+                method='power_iteration',
+                n_components=2,
+                max_power_iter=200,
+            )
 
-        model.fit(X)
-        X_transformed = model.transform(X)
-        X_variances = model._variances
-        X_reconstructed = model.inverse_transform(X_transformed)
+            model.fit(X)
+            X_transformed = model.transform(X)
+            X_variances = model._variances
+            X_reconstructed = model.inverse_transform(X_transformed)
 
-        return X_transformed, X_variances, X_reconstructed
-
-    try:
-        # bandwidth and latency only work for docker mode
-        emulator = emulation.Emulator(
-            emulation.CLUSTER_ABY3_3PC, mode, bandwidth=300, latency=20
-        )
-        emulator.up()
+            return X_transformed, X_variances, X_reconstructed
 
         # Create a simple dataset
         X = random.normal(random.PRNGKey(0), (10, 20))
@@ -79,9 +73,60 @@ def emul_powerPCA(mode: emulation.Mode.MULTIPROCESS):
 
         np.testing.assert_allclose(X_reconstructed, result[2], atol=1e-3)
 
+    def emul_jacobi_PCA():
+        print("start jacobi method emulation.")
+
+        def proc_transform(X):
+            model = PCA(
+                method='serial_jacobi_iteration',
+                n_components=4,
+                max_jacobi_iter=5,
+            )
+
+            model.fit(X)
+            X_transformed = model.transform(X)
+            X_variances = model._variances
+            X_reconstructed = model.inverse_transform(X_transformed)
+
+            return X_transformed, X_variances, X_reconstructed
+
+        # Create a simple dataset
+        X = random.normal(random.PRNGKey(0), (10, 20))
+        X_spu = emulator.seal(X)
+        result = emulator.run(proc_transform)(X_spu)
+
+        # The mean of the transformed data should be approximately 0
+        assert jnp.allclose(jnp.mean(result[0], axis=0), 0, atol=1e-3)
+
+        # Compare with sklearn
+        model = SklearnPCA(n_components=4)
+        model.fit(X)
+        X_transformed_sklearn = model.transform(X)
+        X_variances = model.explained_variance_
+
+        # Compare the transform results(omit sign)
+        np.testing.assert_allclose(
+            np.abs(X_transformed_sklearn), np.abs(result[0]), rtol=0.1, atol=0.1
+        )
+
+        # Compare the variance results
+        np.testing.assert_allclose(X_variances, result[1], rtol=0.1, atol=0.1)
+
+        X_reconstructed = model.inverse_transform(X_transformed_sklearn)
+
+        np.testing.assert_allclose(X_reconstructed, result[2], atol=0.1)
+
+    try:
+        # bandwidth and latency only work for docker mode
+        emulator = emulation.Emulator(
+            emulation.CLUSTER_ABY3_3PC, mode, bandwidth=300, latency=20
+        )
+        emulator.up()
+        emul_powerPCA()
+        emul_jacobi_PCA()
     finally:
         emulator.down()
 
 
 if __name__ == "__main__":
-    emul_powerPCA(emulation.Mode.MULTIPROCESS)
+    test_pca(emulation.Mode.MULTIPROCESS)
