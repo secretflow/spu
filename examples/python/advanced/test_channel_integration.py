87cdb1c200e3dcef8a58549ae6bf87dd93f21b0c
zhsu <suzhihong.szh@antgroup.com>
2025-10-23 10:54:50 +0800

    add licence

--- examples/python/advanced/test_channel_integration.py
+++ examples/python/advanced/test_channel_integration.py
@@ -1,7 +1,16 @@
-#!/usr/bin/env python3
-"""
-Test script: Verify Python class implementing IChannel and passing to C++
-"""
+# Copyright 2025 Ant Group Co., Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import spu.libspu.link as link
 import spu.libspu as spu
